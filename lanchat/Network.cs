73a5e848198329711fe79ab7a40bb4a9be87638c
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2025-03-12 10:04:20 +0100

    lazy additions to networking

--- lanchat/Network.cs
+++ lanchat/Network.cs
@@ -4,5 +4,22 @@ static class Networker
 //func to broadcast public key
 //listen to broadcasts on same port number
 
+ private int Broadcast_port;
+ private int Msg_port;
 
+  public void SendBroadcast(string Usrname, string PublicKey) // so user has a default username to show for the public key it finds
+    {
+        using (UdpClient udpClient = new UdpClient())
+        {
+            udpClient.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, Broadcast_port);
+            byte[] data = Encoding.UTF8.GetBytes(Usrname + "::" + PublicKey); // idk about this seperator, lmk if theres better idea
+
+            udpClient.SendAsync(data, data.Length, endPoint); //async
+            Console.WriteLine($"Broadcast sent"); // remove later, debugging uses
+        }
+    }
+
+// here methods for key exchange and encrypted data transfer
+  
 }
