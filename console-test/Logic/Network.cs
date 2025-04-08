b064942d1b9182da7920ae5ad0c3b5a33016f91d
marc715b <marc715b@mail.hansenberg.dk>
2025-04-08 14:38:20 +0200

    made broadcast listener into asynchronous generator model + added contact object

--- console-test/Logic/Network.cs
+++ console-test/Logic/Network.cs
@@ -9,6 +9,65 @@ using System.Threading.Tasks;
 
 namespace console_test.Logic
 {
+  internal class Contact
+  {
+    private static readonly int MAX_NAME_LEN = 32;
+    private static readonly int ECDH_PUBKEY_LEN = 65; // NOTE: ECDH pub key represented as hex = 65 letters
+                                                      // TODO: move into Crypto.cs file later
+
+    private string _name;
+    private string _ip;
+    private string _pubKey;
+
+    public Contact(string name, string ip, string pubKey)
+    {
+      // Set all fields with input verification
+      SetName(name);
+      SetIp(ip);
+      SetPubKey(pubKey);
+    }
+
+    public void SetName(string name)
+    {
+      if (name.Length > MAX_NAME_LEN)
+        throw new ArgumentException("Contact display name is too long");
+
+      _name = name;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetIp(string ip)
+    {
+      IPAddress tempAddress;
+      if (!IPAddress.TryParse(ip, out tempAddress))
+        throw new ArgumentException("IP address is invalid");
+
+      _ip = ip;
+    }
+
+    public string GetIp()
+    {
+      return _ip;
+    }
+
+    public void SetPubKey(string pubKey)
+    {
+      if (pubKey.Length != ECDH_PUBKEY_LEN)
+        throw new ArgumentException("Invalid public key length");
+
+      _pubKey = pubKey;
+    }
+
+    public string GetPubKey()
+    {
+      return _pubKey;
+    }
+  }
+
   internal class Network
   {
     private static readonly int BROADCAST_PORT = 420;
@@ -45,7 +104,7 @@ namespace console_test.Logic
       }
     }
 
-    public void ListenBroadcast()
+    public async Task<Contact> ListenBroadcast()
     {
       Console.WriteLine($"Listening for broadcasts on port {BROADCAST_PORT}");
 
@@ -71,8 +130,15 @@ namespace console_test.Logic
 
         string[] entries = msg.Split("::");
 
+        string ip = remoteIp.Address.ToString();
+        string user = entries[0];
+        string pubKey = entries[1];
+
         // Print some debug information
-        Console.WriteLine($"Received broadcast: {msg}\n\tSender: {remoteIp.ToString()}\n\tUsername: {entries[0]}\n\tPub key: {entries[1]}");
+        Console.WriteLine($"Received broadcast: {msg}\n\tSender: {remoteIp.ToString()}\n\tUsername: {user}\n\tPub key: {pubKey}");
+
+        // Return a new Contact object for this user
+        return new Contact(user, ip, pubKey);
       }
     }
   }
--- console-test/Program.cs
+++ console-test/Program.cs
@@ -9,16 +9,20 @@ namespace console_test
     public static void Main(string[] args)
     {
       // Start listening for UDP broadcasts
-      Task listenerTask = Task.Run(() =>
+      Task listenerTask = Task.Run(async () =>
       {
-        _network.ListenBroadcast();
+        while (true)
+        {
+          Contact contact = await _network.ListenBroadcast();
+          Console.WriteLine($"Got new contact:\n\tName: {contact.GetName()}\n\tPub key: {contact.GetPubKey()}\n\tIP: {contact.GetIp()}");
+        }
       });
 
       // Send out a test broadcast
-      _network.Broadcast("TestUsername123", "NnQ5ZXZUalVjeWJ0djA1N2plaHpuWUtDU2thSVh5TlA=");
+      _network.Broadcast("TestUsername123", "28701b10ed531c2107f973954e3dde5f28dcc07c06f211c385e6be8a5defe3107");
 
       // Send out another test broadcast
-      _network.Broadcast("1DebugAccount", "Zmo3MnlmQ0F2VTQ0a3RMSFplNWtQckF2Y2NCYWE0eUk=");
+      // _network.Broadcast("1DebugAccount", "90894f84e80fe4561530d68da3bae3d42bb750a3e336a62c2b6a6e0a58bfc11ae");
 
       listenerTask.Wait();
     }
